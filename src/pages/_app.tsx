9ca31a9f27558a2161fca302b2a8903fe4b7f349
VitinhoSouza <vitinhosouzajuatama@gmail.com>
2022-10-04 23:02:06 -0300

    feat: implementing github login with next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,19 @@
 import {AppProps} from 'next/app';
 import Head from 'next/head';
 import { Header } from '../components/Header';
+import {SessionProvider} from "next-auth/react"
 
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps }, }) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Head>
         <title>ig.news</title>
       </Head>
       <Header/>
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
     
   )
 }
